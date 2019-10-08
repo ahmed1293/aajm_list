d8b0d8eb40971da773389fa8a493ea70fc1b5b15
Ahmed <ahmed.a.1293@gmail.com>
2019-10-08 10:04:31 +0100

    move new list button to separate dir

--- frontend/src/components/Tiles.js
+++ frontend/src/components/Tiles.js
@@ -1,7 +1,7 @@
 import React from "react";
 import key from "weak-key";
 import Table from "./Table";
-import {fetchDjango} from "../util";
+import NewListButton from "./buttons/NewList";
 
 class Tiles extends React.Component {
     constructor(props) {
@@ -50,36 +50,6 @@ class Tiles extends React.Component {
                 </div>
             </section>
         </div>
-    }
-}
-
-class NewListButton extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.createList = this.createList.bind(this);
-    }
-
-    createList() {
-        return fetchDjango('/api/shopping-lists/', {
-            method: 'POST',
-            body: {
-                "name": "TEST", // TODO: make editable
-                "created_by": 1, // TODO: get from request
-            }
-        }).then(() => {
-            this.props.updateLists();
-        });  // TODO: handle errors
-    }
-
-    render() {
-        return <section className="section">
-            <div className="container">
-                <a className="button is-dark is-large is-fullwidth has-text-warning" onClick={this.createList}>
-                    New List
-                </a>
-            </div>
-        </section>
     }
 }
 
