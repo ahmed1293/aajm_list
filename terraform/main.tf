cc5bdf819a621e810bb3ba2b5573c5c4170ea98f
Ahmed <ahmed.a.1293@gmail.com>
2019-10-10 23:36:41 +0100

    ec2 security group for ssh

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,11 +7,37 @@ resource "aws_instance" "aajm" {
   ami = var.ami
   instance_type = "t2.micro"
   key_name = var.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 }
 
 resource "aws_eip" "aajm_ip" {
   vpc = true
   instance = aws_instance.aajm.id
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name = "allow_ssh"
+  description = "All SSH connections on port 22"
+
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
 }
 
 output "ip" {
