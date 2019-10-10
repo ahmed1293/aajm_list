6349fe96d4eea753a14eedd429684912035a6371
Ahmed <ahmed.a.1293@gmail.com>
2019-10-10 11:49:11 +0100

    redirect authenticted users

--- frontend/views.py
+++ frontend/views.py
@@ -1,12 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 
 
+@login_required()
 def index(request):
     return render(request, 'frontend/index.html')
 
 
 class Login(LoginView):
-
     template_name = 'frontend/login.html'
+    redirect_authenticated_user = True
 
