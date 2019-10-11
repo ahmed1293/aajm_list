65218d1dacdac42086048756053c2b2e4833a02a
Ahmed <ahmed.a.1293@gmail.com>
2019-10-11 13:12:59 +0100

    fix default settings in manage.py

--- aajm_list/settings/base.py
+++ aajm_list/settings/base.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = '1gwsu2$(1yvmv=paqq$hka6t@!noi*f!m#hq^s+vfd+82-j5v-'
 
 ALLOWED_HOSTS = []
 
