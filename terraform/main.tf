640ba02618cdcc3e14e625631bde24cd6d0b2034
Ahmed <ahmed.a.1293@gmail.com>
2019-10-11 09:53:35 +0100

    terraform ec2 security groups ports 80 and 443

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,26 @@ resource "aws_security_group" "allow_ssh" {
   description = "All SSH connections on port 22"
 
   ingress {
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  ingress {
+    from_port = 443
+    protocol = "tcp"
+    to_port = 443
+
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
+  ingress {
     from_port = 22
     protocol = "tcp"
     to_port = 22
