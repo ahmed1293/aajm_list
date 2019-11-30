ac0e325aea37697505a99c87ffbe28ec626e5736
Ahmed <ahmed.a.1293@gmail.com>
2019-11-30 12:55:40 +0000

    django settings for S3

--- aajm_list/settings/local.py
+++ aajm_list/settings/local.py
@@ -1,8 +1,20 @@
+import socket
+
 from .base import *
 
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/static/'),
+]
 
 # trick to have debug toolbar when developing with docker
 ip = socket.gethostbyname(socket.gethostname())
--- aajm_list/settings/production.py
+++ aajm_list/settings/production.py
@@ -3,3 +3,18 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['3.9.218.51', 'aa-jm.com']
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = 'aajm-s3'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/static/'),
+]
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
